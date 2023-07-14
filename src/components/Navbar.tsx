92b62811c8089bb60be7ecce7ab6438a23869dd1
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-14 07:57:49 -0600

    feat: creation of Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav className="container mx-auto p-2 flex justify-between">
+      <a href="" className="font-bold text-gray-700 text-2xl">
+        OpenTable
+      </a>
+
+      <div className="flex">
+        <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+          Sign in
+        </button>
+        <button className="border p-1 px-4 rounded">Sign up</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
