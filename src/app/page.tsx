e9c6d163cd73b40f2f33e821eb09f76f8fc256d7
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-14 16:04:00 -0600

    refactor: adding children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from '@/components/Hero'
 export default function Home() {
   return (
     <div className="bg-white container mx-auto">
-      <Hero />
+      <Hero>Find your table for any occasion</Hero>
       {/* CARDS */}
       <div className="py-3 mt-10 flex flex-wrap justify-center">
         <Card />
