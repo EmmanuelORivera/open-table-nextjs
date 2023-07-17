23f2ac9e55e5c558c4d4c44cfb657ed3cfe9e73e
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-17 12:41:18 -0600

    feat: creation of a SearchBar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,21 @@
+interface SearchBarProps {
+  placeholder?: string
+}
+const SearchBar: React.FC<SearchBarProps> = ({
+  placeholder = 'State, city or town',
+}) => {
+  return (
+    <div className="flex flex-col gap-4 w-[90%] max-w-3xl sm:flex-row mx-auto">
+      <input
+        className="rounded p-2 flex-grow"
+        type="text"
+        placeholder={placeholder}
+      />
+      <button className="rounded bg-red-600  py-2 text-white sm:w-32 sm:max-w-md">
+        Let's go
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar
