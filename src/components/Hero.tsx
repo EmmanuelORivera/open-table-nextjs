94e4a8df5ef8a5a5a48e7e85d2e67d1ea8fdb007
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-17 18:15:52 -0600

    style: fixed padding to the Hero container

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,12 +7,8 @@ const Hero = ({
   title?: string
   children?: React.ReactNode
 }) => {
-  const sectionVerticalPadding = children ? 'py-20' : 'py-8'
-
   return (
-    <section
-      className={`${sectionVerticalPadding} bg-gradient-to-r from-[#0f1f47] to-[#5f6984]`}
-    >
+    <section className={`py-16 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]`}>
       <div className="text-center">
         {title && (
           <h1 className="text-white text-5xl font-bold mb-10 sm:mb-5 h-20">
