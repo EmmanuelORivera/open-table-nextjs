bc7929f0e1cc35b82209adc219e6ac73e27f1be1
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-18 10:14:05 -0600

    refactor: removing main tag and styling for mobile view.

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -4,9 +4,7 @@ const MenuPage = () => {
   return (
     <section className="bg-gray-100 min-h-screen">
       <div className="max-w-screen-2xl m-auto bg-white">
-        <Hero title="Milestones Grill (Toronto)">
-          <h1 className=""></h1>
-        </Hero>
+        <Hero title="Milestones Grill (Toronto)"></Hero>
         {/* HEADER */} {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[100%] rounded p-3 shadow">
@@ -22,14 +20,14 @@ const MenuPage = () => {
               </a>
             </nav>
             {/* RESAURANT NAVBAR */} {/* MENU */}
-            <main className="bg-white mt-5">
+            <div className="bg-white mt-5">
               <div>
                 <div className="mt-4 pb-1 mb-1">
                   <h1 className="font-bold text-4xl">Menu</h1>
                 </div>
                 <div className="flex flex-wrap justify-between">
                   {/* MENU CARD */}
-                  <div className=" border rounded p-3 w-[49%] mb-3">
+                  <div className=" border rounded p-3 w-full md:w-[49%] mb-3">
                     <h3 className="font-bold text-lg">Surf and Turf</h3>
                     <p className="font-light mt-1 text-sm">
                       A well done steak with lobster and rice
@@ -39,7 +37,7 @@ const MenuPage = () => {
                   {/* MENU CARD */}
                 </div>
               </div>
-            </main>
+            </div>
             {/* MENU */}
           </div>
         </div>
