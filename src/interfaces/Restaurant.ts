dc97ba657f604b6eea2bf5bd717ba933736be427
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-21 11:25:09 -0600

    feat: Restaurant interface was created

---
+++ src/interfaces/Restaurant.ts
@@ -0,0 +1,10 @@
+import { Cuisine, Location, Price } from '@prisma/client'
+
+export interface Restaurant {
+  id: number
+  name: string
+  main_image: string
+  cuisine: Cuisine
+  location: Location
+  price: Price
+}
