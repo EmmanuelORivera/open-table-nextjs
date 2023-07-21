f99867ebe27ae6b1853ebb78efd08d8ce87e4e67
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-21 11:38:13 -0600

    feat: restaurants fetching logic.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import RestaurantCard from '@/components/RestaurantCard'
 import Hero from '@/components/Hero'
 import SearchBar from '@/components/SearchBar'
+import {
+  RestaurantService,
+  PrismaRestaurantService,
+} from '@/services/RestaurantService'
+import { Restaurant } from '@/interfaces/Restaurant'
 
-export default function Home() {
+export default async function Home() {
+  const restaurantService: RestaurantService = new PrismaRestaurantService()
+
+  const restaurants: Restaurant[] = await restaurantService.fetchRestaurants()
+
   return (
     <div>
       <Hero title="Find your table for any occasion">
         <SearchBar />
       </Hero>
       <div className="py-3 px-36 mt-10 flex flex-wrap ">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
       </div>
     </div>
   )
