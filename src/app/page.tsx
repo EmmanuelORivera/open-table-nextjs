47051d35e192970aff1ef45f1a51a7b1fa63317c
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 10:13:13 -0600

    refactor: now using the singleton to get the data, fixing styles and adding key to the map.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Hero from '@/components/Hero'
 import SearchBar from '@/components/SearchBar'
 import { PrismaRestaurantService } from '@/services/PrismaRestaurantService'
 import { Restaurant } from '@/interfaces/Restaurant'
-import { RestaurantService } from '@/interfaces/RestaurantService'
 
 export default async function Home() {
-  const restaurantService: RestaurantService = new PrismaRestaurantService()
+  const restaurantService: PrismaRestaurantService =
+    PrismaRestaurantService.getInstance()
 
   const restaurants: Restaurant[] = await restaurantService.fetchRestaurants()
 
@@ -15,9 +15,9 @@ export default async function Home() {
       <Hero title="Find your table for any occasion">
         <SearchBar />
       </Hero>
-      <div className="py-3 px-36 mt-10 flex flex-wrap ">
+      <div className="py-3 mt-10 flex flex-wrap justify-center gap-6">
         {restaurants.map((restaurant) => (
-          <RestaurantCard restaurant={restaurant} />
+          <RestaurantCard restaurant={restaurant} key={restaurant.id} />
         ))}
       </div>
     </div>
