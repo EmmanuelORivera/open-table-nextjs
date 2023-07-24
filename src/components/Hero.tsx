d99cb613f9ef4b702ec8ff34c4dac684171b99f1
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 13:05:56 -0600

    feat: now the title on the Hero component  is rendered dynamically

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,3 @@
-import SearchBar from './SearchBar'
-
 const Hero = ({
   children,
   title = '',
@@ -7,12 +5,18 @@ const Hero = ({
   title?: string
   children?: React.ReactNode
 }) => {
+  const renderTitle = () => {
+    const nameArray = title.split('-')
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`
+
+    return nameArray.join(' ')
+  }
   return (
     <section className={`py-16 bg-gradient-to-r from-[#0f1f47] to-[#5f6984]`}>
       <div className="text-center">
         {title && (
-          <h1 className="text-white text-4xl font-bold mb-10 sm:mb-5 h-20">
-            {title}
+          <h1 className="text-white text-4xl capitalize font-bold mb-10 sm:mb-5 h-20">
+            {renderTitle()}
           </h1>
         )}
         {children}
