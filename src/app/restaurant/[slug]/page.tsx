9a2b7b53cc89b5cff6c9ae59d8f1d341859de524
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 11:15:46 -0600

    feat: fetching data and passing it as a prop

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -5,14 +5,17 @@ import Rating from './components/Rating'
 import Description from './components/Description'
 import Images from './components/Images'
 import Reviews from './components/Reviews'
+import { PrismaRestaurantService } from '@/services/PrismaRestaurantService'
 
-const RestaurantDetails = () => {
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+  const restaurantService = PrismaRestaurantService.getInstance()
+  const restaurant = await restaurantService.fetchRestaurantBySlug(params.slug)
   return (
     <>
       <div className="px-4 lg:grid lg:grid-cols-[repeat(2,minmax(200px,550px))] lg:gap-10 justify-end">
         <div className="-mt-11 mx-auto max-w-screen-md lg:max-w-screen-md">
           <div className="bg-white mx-auto rounded p-3 shadow text-sm">
-            <RestaurantTabs />
+            <RestaurantTabs slug={restaurant.slug} />
             <Title title="Milesstone Grill" />
             <Rating />
             <Description />
