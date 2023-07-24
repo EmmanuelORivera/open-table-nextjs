36100aed9f5ee274e68210d3b1a7549e737aa83a
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 13:07:36 -0600

    refactor: descriptions is not longer a static valuie

--- src/app/restaurant/[slug]/components/Description.tsx
+++ src/app/restaurant/[slug]/components/Description.tsx
@@ -1,13 +1,7 @@
-const Description = () => {
+const Description = ({ description }: { description: string }) => {
   return (
     <div className="mt-4">
-      <p className="font-medium">
-        The classics you love prepared with a perfect twist, all served up in an
-        atmosphere that feels just right. That’s the Milestones promise. So,
-        whether you’re celebrating a milestone, making the most of Happy Hour or
-        enjoying brunch with friends, you can be sure that every Milestones
-        experience is a simple and perfectly memorable one.
-      </p>
+      <p className="font-medium">{description}</p>
     </div>
   )
 }
