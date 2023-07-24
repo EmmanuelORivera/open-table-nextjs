1d159d645263f45b922628d5ce16eae21fc299ab
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 10:14:49 -0600

    feat: adding a new method to the RestaurantService method.

--- src/interfaces/RestaurantService.ts
+++ src/interfaces/RestaurantService.ts
@@ -1,5 +1,7 @@
+import { Restaurant as PrismaRestaurant } from '@prisma/client'
 import { Restaurant } from './Restaurant'
 
 export interface RestaurantService {
   fetchRestaurants: () => Promise<Restaurant[]>
+  fetchRestaurant: (slug: string) => Promise<PrismaRestaurant | null>
 }
