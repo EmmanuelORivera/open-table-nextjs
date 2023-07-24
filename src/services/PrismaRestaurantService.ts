cc8d17b50b56f18ad024a8df25ea598175cfd25f
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 10:10:02 -0600

    feat: adding a singleton pattern to the class, also creation of method fetchRestaurant

--- src/services/PrismaRestaurantService.ts
+++ src/services/PrismaRestaurantService.ts
@@ -1,12 +1,21 @@
 import { Restaurant } from '@/interfaces/Restaurant'
+import { Restaurant as PrismaRestaurant } from '@prisma/client'
 import { RestaurantService } from '@/interfaces/RestaurantService'
 import { PrismaClient } from '@prisma/client'
 
 export class PrismaRestaurantService implements RestaurantService {
+  private static instance: PrismaRestaurantService
   private prisma: PrismaClient
 
   constructor() {
     this.prisma = new PrismaClient()
+  }
+
+  public static getInstance(): PrismaRestaurantService {
+    if (!PrismaRestaurantService.instance) {
+      PrismaRestaurantService.instance = new PrismaRestaurantService()
+    }
+    return PrismaRestaurantService.instance
   }
 
   async fetchRestaurants(): Promise<Restaurant[]> {
@@ -23,5 +32,15 @@ export class PrismaRestaurantService implements RestaurantService {
     })
 
     return restaurants
+  }
+
+  async fetchRestaurant(slug: string): Promise<PrismaRestaurant | null> {
+    const restaurant = await this.prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+    })
+
+    return restaurant
   }
 }
