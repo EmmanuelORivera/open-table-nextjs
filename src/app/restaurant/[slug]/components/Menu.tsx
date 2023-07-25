0d523fd48399288950a2a45ce030144aa84fdcbb
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 18:24:55 -0600

    feat: a list of the menu items is displayed

--- src/app/restaurant/[slug]/components/Menu.tsx
+++ src/app/restaurant/[slug]/components/Menu.tsx
@@ -1,12 +1,21 @@
 import MenuCard from './MenuCard'
 import Title from './Title'
+import { RestaurantMenu } from '@/interfaces/Restaurant'
 
-const Menu = () => {
+const Menu = ({ menuItems }: { menuItems: RestaurantMenu }) => {
   return (
     <div className="bg-white mt-5">
       <div>
         <Title className="mb-7" title="Menu" />
-        <MenuCard />
+        <div className="md:grid md:grid-cols-2 gap-3">
+          {menuItems.items.length ? (
+            menuItems.items.map((item) => (
+              <MenuCard item={item} key={item.id} />
+            ))
+          ) : (
+            <p>This Restaurant does not have a menu</p>
+          )}
+        </div>
       </div>
     </div>
   )
