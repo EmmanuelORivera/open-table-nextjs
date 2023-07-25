661d930ae2370f84ebd23c1ed1f566c50001626a
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-24 18:27:38 -0600

    feat: fetching data on MenuPage server component

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,18 +1,21 @@
 import RestaurantTabs from '../components/RestaurantTabs'
 import Menu from '../components/Menu'
 import { Metadata } from 'next'
+import { PrismaRestaurantService } from '@/services/PrismaRestaurantService'
 
 export const metadata: Metadata = {
   title: 'Menu of Milesones Grill | OpenTable',
 }
 
-const MenuPage = ({ params }: { params: { slug: string } }) => {
+const MenuPage = async ({ params }: { params: { slug: string } }) => {
+  const restaurantService = PrismaRestaurantService.getInstance()
+  const menuItems = await restaurantService.fetchRestaurantMenu(params.slug)
   return (
     <>
       <div className="px-4 flex m-auto w-full max-w-screen-lg  justify-between items-start 0 -mt-11">
         <div className="bg-white w-[100%] rounded p-3 shadow text-sm">
           <RestaurantTabs slug={params.slug} />
-          <Menu />
+          <Menu menuItems={menuItems} />
         </div>
       </div>
     </>
