9769bdc2e9ac9cd4ab58fc49f7b105973ab9c442
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-26 17:52:21 -0600

    refactor: now the filter uses filterType to check which key to use.

--- src/app/search/components/ListFilter.tsx
+++ src/app/search/components/ListFilter.tsx
@@ -1,21 +1,24 @@
 import Link from 'next/link'
+import { ListFilterType } from './SearchSideBar'
 interface Props {
   searchParams: { city?: string; cuisine?: string }
-  title: string
+  filterType: ListFilterType
   items: { id: number; name: string }[]
-  generateQuery: (
-    locationOrCuisine: { id: number; name: string },
-    searchParams: { city?: string; cuisine?: string }
-  ) => { city?: string; cuisine?: string }
 }
 
-const ListFilter = ({ searchParams, title, items, generateQuery }: Props) => {
+const ListFilter = ({ searchParams, filterType, items }: Props) => {
   return (
     <div className="border-b pb-4">
-      <h2 className="mb-2">{title}</h2>
+      <h2 className="mb-2">{filterType}</h2>
       {items.map((item) => {
-        const query = generateQuery(item, searchParams)
+        let key = ''
+        if (ListFilterType.Cuisine === filterType) {
+          key = 'cuisine'
+        } else if (ListFilterType.Region === filterType) {
+          key = 'city'
+        }
 
+        const query = { ...searchParams, [key]: item.name }
         return (
           <p className="font-light text-reg capitalize" key={item.id}>
             <Link href={{ pathname: '/search', query }}>{item.name}</Link>
