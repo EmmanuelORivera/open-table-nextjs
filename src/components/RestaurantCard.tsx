c19b1ca5500706f3c6a01fb7ecf2c90ecd9a032d
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-27 16:21:19 -0600

    feat: dynamic review info to the RestaurantCard component

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -3,7 +3,15 @@ import Link from 'next/link'
 import React from 'react'
 import RestaurantPrice from './RestaurantPrice'
 
+const getReviewInfo = (restaurant: Restaurant) => {
+  const numberOfReviews = restaurant.reviews.length
+  const reviewText = numberOfReviews === 1 ? 'review' : 'reviews'
+
+  return { numberOfReviews, reviewText }
+}
 const RestaurantCard = ({ restaurant }: { restaurant: Restaurant }) => {
+  const { numberOfReviews, reviewText } = getReviewInfo(restaurant)
+
   return (
     <div className="pb-3 w-full max-w-[270px] rounded overflow-hidden border cursor-pointer">
       <Link href={`/restaurant/${restaurant.slug}`}>
@@ -16,7 +24,9 @@ const RestaurantCard = ({ restaurant }: { restaurant: Restaurant }) => {
           <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
-            <p className="ml-2">77 reviews</p>
+            <p className="ml-2">
+              {numberOfReviews} {reviewText}
+            </p>
           </div>
           <div className="flex font-light capitalize">
             <p className=" mr-3">{restaurant.cuisine.name}</p>
