b19b9c392a2ac9010ab8eda438a6c1c52ddf0f26
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-27 11:32:16 -0600

    refactor: adding className to the price object

--- src/app/search/components/PriceFilter.tsx
+++ src/app/search/components/PriceFilter.tsx
@@ -10,18 +10,21 @@ const PriceFilter = ({
     price?: string
   }
 }) => {
-  const prices: { price: Price; label: string }[] = [
+  const prices: { price: Price; label: string; className: string }[] = [
     {
       price: Price.CHEAP,
       label: '$',
+      className: 'border w-full text-reg text-center font-light rounded-l p-2',
     },
     {
       price: Price.REGULAR,
       label: '$$',
+      className: 'border w-full text-reg text-center font-light p-2',
     },
     {
       price: Price.EXPENSIVE,
       label: '$$$',
+      className: 'border w-full text-reg text-center font-light rounded-r p-2',
     },
   ]
 
@@ -29,14 +32,14 @@ const PriceFilter = ({
     <div className="mt-3 pb-4">
       <h2 className="mb-2">Price</h2>
       <div className="flex">
-        {prices.map(({ price, label }) => (
+        {prices.map(({ price, label, className }) => (
           <Link
             key={label}
             href={{
               pathname: '/search',
               query: { ...searchParams, price },
             }}
-            className="border w-full text-reg font-light rounded-l p-2"
+            className={className}
           >
             {label}
           </Link>
