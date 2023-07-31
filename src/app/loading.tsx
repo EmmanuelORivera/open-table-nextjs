f1547196bcfa938bf61598b0089e060bec0796a7
Emmanuel Ochoa <eorivera95@gmail.com>
2023-07-31 08:52:19 -0600

    feat: a loading component with pulse animation

---
+++ src/app/loading.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/components/Hero'
+
+const loading = () => {
+  return (
+    <section>
+      <Hero />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
+          <div
+            key={num}
+            className="animate-pulse m-3 bg-slate-200 w-64 h-72 rounded overflow-hidden border cursor-pointer"
+          ></div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default loading
