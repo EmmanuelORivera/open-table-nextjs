bb9b0b9ea88e0d4aa498428252535377f0860e44
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-07 12:53:55 -0600

    refactor: now the auth comes from a middleware

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -5,33 +5,7 @@ import { PrismaUserService } from '@/services/PrismaUserService'
 import { UserService } from '@/interfaces/UserService'
 
 export async function GET(req: NextRequest) {
-  const headersList = headers()
-  const bearerToken = headersList.get('authorization')
-
-  if (!bearerToken) {
-    return NextResponse.json(
-      { errorMessage: 'Unauthorized request' },
-      { status: 401 }
-    )
-  }
-
-  const token = bearerToken?.split(' ')[1] as string
-
-  if (!token) {
-    return NextResponse.json(
-      { errorMessage: 'Unauthorized request' },
-      { status: 401 }
-    )
-  }
-
-  const isTokenValid = await AuthService.verifyToken(token)
-
-  if (!isTokenValid) {
-    return NextResponse.json(
-      { errorMessage: 'Unauthorized request' },
-      { status: 401 }
-    )
-  }
+  const token = headers().get('authorization') as string
 
   const payload = AuthService.decodeJwt(token)
 
