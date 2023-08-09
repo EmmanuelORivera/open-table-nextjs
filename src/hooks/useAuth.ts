8e4994fca08ae032a08426c77d1aa03e7ba15c9b
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-09 13:12:58 -0600

    feat: adding logic to the loading state

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,9 @@
+import { useAuthContext } from '@/context/AuthContext'
 import axios from 'axios'
 
 const useAuth = () => {
+  const { setAuthState } = useAuthContext()
+
   const signin = async ({
     email,
     password,
@@ -8,17 +11,22 @@ const useAuth = () => {
     email: string
     password: string
   }) => {
+    setAuthState({ data: null, error: null, loading: true })
     try {
-      const response = await axios.post<{ token: string }>(
+      const response = await axios.post(
         'http://localhost:3000/api/auth/signin',
         {
           email,
           password,
         }
       )
-      console.log(response)
-    } catch (error) {
-      console.log(error)
+      setAuthState({ data: response.data, error: null, loading: false })
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      })
     }
   }
   const signup = async () => {}
