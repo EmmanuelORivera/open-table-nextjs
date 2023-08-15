172b9694ffd0677289497fe050825ba19f3d4188
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-15 15:08:36 -0600

    feat: get endpoint for the availability

---
+++ src/app/api/restaurant/[slug]/availability/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url)
+  const pathSegment = url.pathname.split('/')
+  const slug = pathSegment[pathSegment.length - 1]
+
+  //query strings
+  const day = url.searchParams.get('day')
+  const time = url.searchParams.get('time')
+  const partySize = url.searchParams.get('partySize')
+
+  if (!day || !time || !partySize) {
+    return NextResponse.json(
+      { errorMessage: 'Invalid data provided' },
+      { status: 400 }
+    )
+  }
+
+  return NextResponse.json({ slug, day, time, partySize })
+}
