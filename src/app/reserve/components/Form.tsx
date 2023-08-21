9dc1ba711eb990288e99d6f147a283469898fa1e
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-21 16:21:17 -0600

    feat: now the Form component can be disabled

--- src/app/reserve/components/Form.tsx
+++ src/app/reserve/components/Form.tsx
@@ -1,40 +1,99 @@
+'use client'
+
+import Button from '@/components/Button'
+import { useEffect, useState } from 'react'
+
 const Form = () => {
+  const [inputs, setInputs] = useState({
+    bookerFirstName: '',
+    bookerLastName: '',
+    bookerPhone: '',
+    bookerEmail: '',
+    bookerOccasion: '',
+    bookerRequests: '',
+  })
+
+  const [disabled, setDisabled] = useState(true)
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  useEffect(() => {
+    if (
+      inputs.bookerFirstName &&
+      inputs.bookerLastName &&
+      inputs.bookerPhone &&
+      inputs.bookerEmail
+    ) {
+      setDisabled(false)
+    } else {
+      setDisabled(true)
+    }
+  }, [inputs])
+
   return (
     <div className="mt-10 grid md:grid-cols-2 gap-4  w-full">
       <input
         type="text"
+        name="bookerFirstName"
+        onChange={handleChangeInput}
+        value={inputs.bookerFirstName}
         className="border rounded p-3 w-full"
         placeholder="First name"
       />
       <input
         type="text"
+        name="bookerLastName"
+        onChange={handleChangeInput}
+        value={inputs.bookerLastName}
         className="border rounded p-3 w-full"
         placeholder="Last name"
       />
 
       <input
         type="text"
+        name="bookerPhone"
+        onChange={handleChangeInput}
+        value={inputs.bookerPhone}
         className="border rounded p-3 w-full"
         placeholder="Phone number"
       />
       <input
         type="text"
+        name="bookerEmail"
+        onChange={handleChangeInput}
+        value={inputs.bookerEmail}
         className="border rounded p-3 w-full"
         placeholder="Email"
       />
       <input
         type="text"
+        name="bookerOccasion"
+        onChange={handleChangeInput}
+        value={inputs.bookerOccasion}
         className="border rounded p-3 w-full"
         placeholder="Occasion (optional)"
       />
       <input
         type="text"
+        name="bookerRequests"
+        onChange={handleChangeInput}
+        value={inputs.bookerRequests}
         className="border rounded p-3 w-full"
         placeholder="Requests (optional)"
       />
-      <button className="md:col-span-2 bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
+      <Button
+        className="md:col-span-2 w-full font-bold"
+        type="action"
+        handleClick={() => {}}
+        disabled={disabled}
+      >
         Complete reservation
-      </button>
+      </Button>
       <p className="md:col-span-2 mt-4 text-sm">
         By clicking “Complete reservation” you agree to the OpenTable Terms of
         Use and Privacy Policy. Standard text message rates may apply. You may
