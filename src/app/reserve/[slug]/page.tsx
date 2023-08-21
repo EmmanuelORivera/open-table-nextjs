fac1c06e713aa98bb35f7ac8dd7b8e2c1ad57f17
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-21 17:41:34 -0600

    feat: using an interface for the params, also passing props to the Form component.

--- src/app/reserve/[slug]/page.tsx
+++ src/app/reserve/[slug]/page.tsx
@@ -1,19 +1,28 @@
 import Form from '../components/Form'
 import Header from '../components/Header'
 
+export interface ReservePageParams {
+  slug: string
+}
+
+export interface ReserveSearchParams {
+  date: string
+  partySize: string
+}
+
 const Reserve = ({
   params,
   searchParams,
 }: {
-  params: { slug: string }
-  searchParams: { date: string; partySize: string }
+  params: ReservePageParams
+  searchParams: ReserveSearchParams
 }) => {
   return (
     <section>
       <div className="border-t h-screen">
         <div className="py-9 w-3/5 m-auto">
           <Header params={params} searchParams={searchParams} />
-          <Form />
+          <Form params={params} searchParams={searchParams} />
         </div>
       </div>
     </section>
