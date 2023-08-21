10774bf41ef5f53954cde7bd51f6c42e0376df4f
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-21 14:20:09 -0600

    feat: params and searchParams are passed as a prop

--- src/app/reserve/[slug]/page.tsx
+++ src/app/reserve/[slug]/page.tsx
@@ -1,12 +1,18 @@
 import Form from '../components/Form'
 import Header from '../components/Header'
 
-const Reserve = () => {
+const Reserve = ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams: { date: string; partySize: string }
+}) => {
   return (
     <section>
       <div className="border-t h-screen">
         <div className="py-9 w-3/5 m-auto">
-          <Header />
+          <Header params={params} searchParams={searchParams} />
           <Form />
         </div>
       </div>
--- src/app/reserve/components/Header.tsx
+++ src/app/reserve/components/Header.tsx
@@ -1,15 +1,30 @@
-const Header = () => {
+import { PrismaRestaurantService } from '@/services/PrismaRestaurantService'
+import { notFound } from 'next/navigation'
+
+const Header = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams: { date: string; partySize: string }
+}) => {
+  console.log(params)
+  console.log(searchParams)
+  const restaurantService = PrismaRestaurantService.getInstance()
+  const restaurant = await restaurantService.fetchRestaurantBySlug(params.slug)
+
+  if (!restaurant) notFound()
   return (
     <div>
       <h3 className="font-bold hidden md:block">You're almost done!</h3>
       <div className="mt-5 flex gap-4">
         <img
-          src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-          alt=""
-          className="w-32 h-18 rounded hidden md:block"
+          src={restaurant.main_image}
+          alt={restaurant.name}
+          className="w-32 h-20 rounded hidden md:block object-cover"
         />
         <div className="">
-          <h1 className="text-3xl font-bold">Aiāna Restaurant Collective</h1>
+          <h1 className="text-3xl font-bold">{restaurant.name}</h1>
           <div className="flex mt-3">
             <p className="mr-6">Tues, 22, 2023</p>
             <p className="mr-6">7:30 PM</p>
