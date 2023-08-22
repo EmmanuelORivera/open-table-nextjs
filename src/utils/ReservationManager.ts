cda416e14a61ba987861ad85a5286edaa768eafc
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-22 14:11:30 -0600

    feat: ReservationManager class was created

---
+++ src/utils/ReservationManager.ts
@@ -0,0 +1,26 @@
+import { Booking } from '@prisma/client'
+
+export class ReservationManager {
+  reservations: Booking[]
+  constructor(reservations: Booking[]) {
+    this.reservations = reservations
+  }
+
+  get todayReservations() {
+    const today = new Date()
+
+    return this.reservations.filter((reservation) => {
+      const reservationDate = new Date(reservation.booking_time)
+
+      return (
+        reservationDate.getDate() === today.getDate() &&
+        reservationDate.getMonth() === today.getMonth() &&
+        reservationDate.getFullYear() === today.getFullYear()
+      )
+    })
+  }
+
+  get todayReservationsCount() {
+    return this.todayReservations.length
+  }
+}
