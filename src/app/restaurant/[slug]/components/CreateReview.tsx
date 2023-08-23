81647ac3eb1dae70ccc2a7c92530128c8986aadb
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-23 12:32:51 -0600

    feat: Preparing the Leave a Review section

--- src/app/restaurant/[slug]/components/CreateReview.tsx
+++ src/app/restaurant/[slug]/components/CreateReview.tsx
@@ -4,16 +4,25 @@ import Title from './Title'
 import InputField from '@/components/InputField'
 import { useState } from 'react'
 import Button from '@/components/Button'
+import StarRating from '@/components/StarRating'
 const CreateReview = () => {
   const { data } = useAuthContext()
   const [value, setValue] = useState('')
+  const [rating, setRating] = useState(5)
 
   return (
     <div>
       {!!data && (
         <div>
-          <Title title="Make a review"></Title>
-
+          <Title title="Leave a Review"></Title>
+          <p className="text-gray-500 my-4">
+            We'd love to hear your thoughts! Your feedback helps us improve and
+            provide better experiences.
+          </p>
+          <p className="flex gap-1 items-center">
+            <span className="font-semibold">Rating (1 Bad, 5 Excellent):</span>
+            <StarRating rating={rating} setRating={setRating} />
+          </p>
           <div className="flex flex-col mt-6 gap-6">
             <InputField
               name="comment"
