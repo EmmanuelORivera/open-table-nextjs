1a8b25f6330c53fb12e7989d7c4178206bf802a4
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-28 15:17:09 -0600

    temp: temporal restaurant id

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -23,6 +23,7 @@ const RestaurantCard = async ({ restaurant }: { restaurant: Restaurant }) => {
   }
   return (
     <div className="pb-3 w-full max-w-[270px] rounded overflow-hidden border cursor-pointer">
+      {restaurant.id}
       {JSON.stringify(reviews)}
       <Link href={`/restaurant/${restaurant.slug}`}>
         <Image
