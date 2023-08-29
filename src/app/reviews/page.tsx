46c41d69ca9b7f9a54d7b2f84d44ffabeb44a9fd
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-28 18:42:58 -0600

    temp: trying to add one review

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,6 +1,17 @@
+import { PrismaReviewService } from '@/services/PrismaReviewService'
 import { prisma } from '@/services/PrismaSingleton'
 
 const Reviews = async () => {
+  const reviewService = PrismaReviewService.getInstance()
+  const createdReview = await reviewService.createReview(
+    'Laith',
+    'Harb',
+    3,
+    'this is a great place to eat',
+    1,
+    1
+  )
+
   const specificRestaurant = await prisma.restaurant.findUnique({
     where: { id: 1 },
     include: { reviews: true },
@@ -9,8 +20,11 @@ const Reviews = async () => {
   const allReviews = await prisma.review.findMany()
   return (
     <div>
-      {JSON.stringify(specificRestaurant?.reviews)}
-      {JSON.stringify(allReviews)}
+      <div className="bg-red-400">
+        {JSON.stringify(specificRestaurant?.reviews)}
+      </div>
+      <div className="bg-green-400">{JSON.stringify(allReviews)}</div>
+      <div className="bg-blue-400">{JSON.stringify(createdReview)}</div>
     </div>
   )
 }
