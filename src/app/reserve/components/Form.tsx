5311b95fc72e04a65ae9a5184ef9226ed8eb3cbb
Emmanuel Ochoa <eorivera95@gmail.com>
2023-08-30 16:32:47 -0600

    feat: alert on error was added, also changing the validation with the strategy pattern.

--- src/app/reserve/components/Form.tsx
+++ src/app/reserve/components/Form.tsx
@@ -5,7 +5,10 @@ import InputField from '@/components/InputField'
 import useReservation from '@/hooks/useReservation'
 import { useEffect, useState } from 'react'
 import { ReservePageParams, ReserveSearchParams } from '../[slug]/page'
-import { CircularProgress } from '@mui/material'
+import { Alert, CircularProgress } from '@mui/material'
+import { ReserveValidationStrategy } from '@/strategies/ReserveValidationStrategy'
+import { ValidationStrategy } from '@/interfaces/ValidationStrategy'
+import { ReserveFormInputs } from '@/interfaces/ReserveFormInputs'
 
 const Form = ({
   params,
@@ -15,12 +18,12 @@ const Form = ({
   searchParams: ReserveSearchParams
 }) => {
   const [inputs, setInputs] = useState({
-    bookerFirstName: '',
-    bookerLastName: '',
-    bookerPhone: '',
-    bookerEmail: '',
-    bookerOccasion: '',
-    bookerRequests: '',
+    first_name: '',
+    last_name: '',
+    phone: '',
+    email: '',
+    occasion: '',
+    request: '',
   })
 
   const [didBook, setDidBook] = useState(false)
@@ -34,19 +37,14 @@ const Form = ({
       [e.target.name]: e.target.value,
     })
   }
+  const reserveValidation: ValidationStrategy<ReserveFormInputs> =
+    new ReserveValidationStrategy()
+
+  const isFormValid = reserveValidation.isValid(inputs)
 
   useEffect(() => {
-    if (
-      inputs.bookerFirstName &&
-      inputs.bookerLastName &&
-      inputs.bookerPhone &&
-      inputs.bookerEmail
-    ) {
-      setDisabled(false)
-    } else {
-      setDisabled(true)
-    }
-  }, [inputs])
+    setDisabled(!isFormValid)
+  }, [inputs, isFormValid])
 
   const [day, time] = searchParams.date.split('T')
 
@@ -56,92 +54,99 @@ const Form = ({
       partySize: searchParams.partySize,
       time,
       day,
-      bookerFirstName: inputs.bookerFirstName,
-      bookerLastName: inputs.bookerLastName,
-      bookerEmail: inputs.bookerEmail,
-      bookerOccasion: inputs.bookerOccasion,
-      bookerPhone: inputs.bookerPhone,
-      bookerRequests: inputs.bookerRequests,
+      bookerFirstName: inputs.first_name,
+      bookerLastName: inputs.last_name,
+      bookerEmail: inputs.email,
+      bookerOccasion: inputs.occasion,
+      bookerPhone: inputs.phone,
+      bookerRequests: inputs.request,
       setDidBook,
     })
   }
 
   return (
-    <div className="mt-10 grid md:grid-cols-2 gap-4  w-full">
-      {didBook ? (
-        <div>
-          <h1>You are all booked up</h1>
-          <p>Enjoy your reservation</p>
-        </div>
-      ) : (
-        <>
-          <InputField
-            type="text"
-            value={inputs.bookerFirstName}
-            placeholder="First name"
-            name="bookerFirstName"
-            onChange={handleChangeInput}
-          />
-
-          <InputField
-            type="text"
-            value={inputs.bookerLastName}
-            placeholder="Last name"
-            name="bookerLastName"
-            onChange={handleChangeInput}
-          />
-          <InputField
-            type="text"
-            value={inputs.bookerPhone}
-            placeholder="Phone number"
-            name="bookerPhone"
-            onChange={handleChangeInput}
-          />
-
-          <InputField
-            type="text"
-            value={inputs.bookerEmail}
-            placeholder="Email"
-            name="bookerEmail"
-            onChange={handleChangeInput}
-          />
-
-          <InputField
-            type="text"
-            value={inputs.bookerOccasion}
-            placeholder="Occasion (optional)"
-            name="bookerOccasion"
-            onChange={handleChangeInput}
-          />
-
-          <InputField
-            type="text"
-            value={inputs.bookerRequests}
-            placeholder="Occasion (optional)"
-            name="bookerRequests"
-            onChange={handleChangeInput}
-          />
-
-          <Button
-            className="md:col-span-2 w-full font-bold"
-            type="action"
-            handleClick={handleClick}
-            disabled={disabled || loading}
-          >
-            {loading ? (
-              <CircularProgress color="inherit" />
-            ) : (
-              'Complete reservation'
-            )}
-          </Button>
-          <p className="md:col-span-2 mt-4 text-sm">
-            By clicking “Complete reservation” you agree to the OpenTable Terms
-            of Use and Privacy Policy. Standard text message rates may apply.
-            You may opt out of receiving text messages at any time.
-          </p>
-        </>
+    <>
+      {error && (
+        <Alert severity="error" className="mt-8">
+          {error}
+        </Alert>
       )}
-    </div>
+      <div className="mt-10 grid md:grid-cols-2 gap-4  w-full">
+        {didBook ? (
+          <div>
+            <h1>You are all booked up</h1>
+            <p>Enjoy your reservation</p>
+          </div>
+        ) : (
+          <>
+            <InputField
+              type="text"
+              value={inputs.first_name}
+              placeholder="First name"
+              name="first_name"
+              onChange={handleChangeInput}
+            />
+
+            <InputField
+              type="text"
+              value={inputs.last_name}
+              placeholder="Last name"
+              name="last_name"
+              onChange={handleChangeInput}
+            />
+            <InputField
+              type="text"
+              value={inputs.phone}
+              placeholder="Phone number"
+              name="phone"
+              onChange={handleChangeInput}
+            />
+
+            <InputField
+              type="text"
+              value={inputs.email}
+              placeholder="Email"
+              name="email"
+              onChange={handleChangeInput}
+            />
+
+            <InputField
+              type="text"
+              value={inputs.occasion}
+              placeholder="Occasion (optional)"
+              name="occasion"
+              onChange={handleChangeInput}
+            />
+
+            <InputField
+              type="text"
+              value={inputs.request}
+              placeholder="Request (optional)"
+              name="request"
+              onChange={handleChangeInput}
+            />
+
+            <Button
+              className="md:col-span-2 w-full font-bold"
+              type="action"
+              handleClick={handleClick}
+              disabled={disabled || loading}
+            >
+              {loading ? (
+                <CircularProgress color="inherit" />
+              ) : (
+                'Complete reservation'
+              )}
+            </Button>
+            <p className="md:col-span-2 mt-4 text-sm">
+              By clicking “Complete reservation” you agree to the OpenTable
+              Terms of Use and Privacy Policy. Standard text message rates may
+              apply. You may opt out of receiving text messages at any time.
+            </p>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
